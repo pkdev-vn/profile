ed21db16517558dca215ea68d1f57ce840c8d8fa
pkdevvn <khaiphu2015@gmail.com>
2026-06-28 01:36:14 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,3 +5,11 @@ app = Flask(__name__, template_folder='../templates')
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
